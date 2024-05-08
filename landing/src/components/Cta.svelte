71f42a2853bd57063f92ff35d42d8a41fc3a3379
xnought <bertuccd@oregonstate.edu>
2024-05-08 13:21:14 -0700

    feat: pilled

--- landing/src/components/Cta.svelte
+++ landing/src/components/Cta.svelte
@@ -8,16 +8,21 @@
 </script>
 
 <div class="flex items-center gap-4">
-	<Button size="lg" color="light" href="https://github.com/xnought/venome"
+	<Button
+		size="lg"
+		color="light"
+		pill
+		href="https://github.com/xnought/venome"
 		>Open Source <GithubSolid class="ml-1" size="lg" />
 	</Button>
 	<Button
 		size="lg"
 		color="light"
+		pill
 		href="https://github.com/Venom-Biochem-Lab/venome-poster/blob/main/posters/2024.TheUnknownVenome.CS.094.pdf"
 		>2024 Engineering Expo Poster <FilePdfSolid class="ml-1" />
 	</Button>
-	<Button size="lg" color="primary" disabled
+	<Button size="lg" color="primary" disabled pill
 		>Get Started (🚧 Coming Soon) <ArrowRightOutline class="ml-1" /></Button
 	>
 </div>
