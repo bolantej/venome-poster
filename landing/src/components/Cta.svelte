a3dcb8decf267ebff8471fc5b6e75f7a9e71ada9
xnought <bertuccd@oregonstate.edu>
2024-05-08 13:15:02 -0700

    fix: get icons to work by reverting version

--- landing/src/components/Cta.svelte
+++ landing/src/components/Cta.svelte
@@ -1,18 +1,23 @@
 <script>
 	import { Button } from "flowbite-svelte";
+	import {
+		GithubSolid,
+		ArrowRightOutline,
+		FilePdfSolid,
+	} from "flowbite-svelte-icons";
 </script>
 
 <div class="flex items-center gap-4">
-	<Button size="lg" color="primary" disabled
-		>Deployed Site (🚧 Coming Soon)</Button
-	>
 	<Button size="lg" color="light" href="https://github.com/xnought/venome"
-		>Source Code</Button
-	>
+		>Open Source <GithubSolid class="ml-1" size="lg" />
+	</Button>
 	<Button
 		size="lg"
 		color="light"
-		href="https://venombiochemistrylab.weebly.com/"
-		>Venom Biochemistry Lab</Button
+		href="https://github.com/Venom-Biochem-Lab/venome-poster/blob/main/posters/2024.TheUnknownVenome.CS.094.pdf"
+		>2024 Engineering Expo Poster <FilePdfSolid class="ml-1" />
+	</Button>
+	<Button size="lg" color="primary" disabled
+		>Get Started (🚧 Coming Soon) <ArrowRightOutline class="ml-1" /></Button
 	>
 </div>
